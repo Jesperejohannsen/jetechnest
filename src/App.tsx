d60bf682598b4862663e169fc6131ad122f59106
Jesperejohannsen <you@example.com>
2023-12-26 11:29:21 +0100

    adding contact section mockup

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import About from './components/about/About';
 import SkillSection from './components/skills/SkillSection';
 import ProjectsSection from './components/projects/ProjectsSection';
+import ContactForm from './components/contact/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <About />
       <SkillSection />
       <ProjectsSection />
+      <ContactForm />
     </Router>
   );
 }
