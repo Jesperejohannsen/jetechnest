7f2f77c39c6d31fbffb96b40d23a03726be420e0
Jesperejohannsen <you@example.com>
2023-12-27 11:29:50 +0100

    changing the skill section

--- src/components/skills/SkillSection.tsx
+++ src/components/skills/SkillSection.tsx
@@ -1,27 +1,25 @@
-import "./SkillSection.css";
-
-const skills = [
-  "MSSQL",
-  "GraphQL",
-  "C#",
-  ".NET",
-  "HTML5",
-  "CSS3",
-  "React",
-  "TypeScript",
-  "Azure Cloud",
-];
-
 const SkillSection = () => {
+  const skills = {
+    frontend: ["HTML5", "CSS3", "TypeScript", "React"],
+    backend: [".NET", "C#", "GraphQL", "MSSQL"],
+    database: ["MongoDB", "MySQL", "PostgreSQL", "SQLite"],
+    cloud: ["Azure"],
+  };
+
   return (
     <div className="skill-section">
-      {skills.map((skill, index) => (
-        <div key={index} className="skill-box">
-          <div className="skill-name">{skill}</div>
+      {Object.entries(skills).map(([category, skills], index) => (
+        <div key={index} className={`category-box ${category}`}>
+          <div className="skill-box">
+            <h3>{category.charAt(0).toUpperCase() + category.slice(1)}</h3>
+          </div>
+          {skills.map((skill, index) => (
+            <div key={index} className="skill-box">
+              <div className="skill-name">{skill}</div>
+            </div>
+          ))}
         </div>
       ))}
     </div>
   );
 };
-
-export default SkillSection;
