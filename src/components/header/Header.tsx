59b6f2dbba14208493a56d8254c55d8eb12c9e0f
Jesperejohannsen <you@example.com>
2024-01-17 18:07:55 +0100

    adding new icons and styling

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
 
 function Header() {
@@ -5,6 +8,17 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">JETechNest</div>
+      <div className="icon-group">
+        <a href="https://github.com/Jesperejohannsen" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} size="2x" color='white' />
+        </a>
+        <a href="https://www.linkedin.com/in/jesper-johannsen-61a140161/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" color='white'/>
+        </a>
+        <a href="mailto:jesperejohannsen@outlook.dk">
+          <FontAwesomeIcon icon={faEnvelope} size="2x" color='white'/>
+        </a>
+      </div>
     </header>
   );
 }
